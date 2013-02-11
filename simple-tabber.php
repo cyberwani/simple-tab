45758543cd1c611b1d1faaa8044852401c4deab2
Dominik Schilling <dominikschilling+git@gmail.com>
2013-02-11 22:30:34 +0100

    Add the new dependency in DS_WP_Simple_Tab::register_script()

--- simple-tabber.php
+++ simple-tabber.php
@@ -11,36 +11,46 @@ License: GPLv2 or later
 
 class DS_WP_Simple_Tab {
 	private static $class;
-	
+
 	public function __construct() {
 		add_action( 'admin_enqueue_scripts', array( __CLASS__, 'register_script' ) );
 		add_action( 'admin_enqueue_scripts', array( __CLASS__, 'enqueue_script' ) );
 		add_action( 'admin_footer-post.php', array( __CLASS__, 'print_script' ) );
 		add_action( 'admin_footer-post-new.php', array( __CLASS__, 'print_script' ) );
 	}
-	
+
 	public static function register_script() {
-		$js = ( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ) ? '/js/jquery.taboverride.js' : '/js/jquery.taboverride.min.js';
+		$suffix = ( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ) ? '.min' : '';
 
 		wp_register_script(
-			'jquery-tab-override',
-			plugins_url( $js, __FILE__ ),
-			array( 'jquery' ),
-			'1.1',
+			'taboverride',
+			plugins_url( "js/taboverride$suffix.js", __FILE__ ),
+			array(),
+			'3.2.1',
+			true
+		);
+
+		wp_register_script(
+			'jquery-taboverride',
+			plugins_url( "js/jquery.taboverride$suffix.js", __FILE__ ),
+			array( 'jquery', 'taboverride' ),
+			'3.2.3',
 			true
 		);
 	}
-	
+
 	public static function enqueue_script( $hook_suffix ) {
 		if ( in_array( $hook_suffix, array( 'post-new.php', 'post.php' ) ) )
-			wp_enqueue_script( 'jquery-tab-override' );
+			wp_enqueue_script( 'jquery-taboverride' );
 	}
-	
+
 	public static function print_script() {
 		?>
 		<script type="text/javascript">
 		( function( $ ) {
-			$( '#content' ).tabOverride();
+			$(document).ready( function() {
+				$( '#content' ).tabOverride();
+			});
 		} )( jQuery );
 		</script>
 		<?php
